f304667609004a832182ea2378064421f96b4e35
stevenjmarsh <sjm.atl@gmail.com>
2016-09-02 14:53:42 -0400

    adding a component that accesses basic github profile info

--- app/index.js
+++ app/index.js
@@ -1,15 +1,48 @@
+var GITHUB_USER_DATA = {
+  name: "Steve Marsh",
+  username: "stevenjmarsh",
+  image: "https://avatars0.githubusercontent.com/u/6202447?v=3&s=140"
+};
+
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
   render: function () {
     return (
-      <div>Hello React!</div>
+      <img src={this.props.imageUrl} style={{height: 100, width: 100}}/>
     );
   }
 });
 
-ReactDOM.render(
-  <HelloWorld />,
-  document.getElementById("app")
-);
+var ProfileLink = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <a href={"https://www.github.com/" + this.props.username}>{this.props.username}</a>
+      </div>
+    );
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function () {
+    return (
+      <div>{this.props.name}</div>
+    );
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name}/>
+        <ProfileLink username={this.props.user.username}/>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<Avatar user={GITHUB_USER_DATA}/>, document.getElementById("app"));
