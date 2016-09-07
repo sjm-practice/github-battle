2e0e38b4df01c50fe1e95cf0ecf6c1fbe23e4355
stevenjmarsh <sjm.atl@gmail.com>
2016-09-07 15:30:23 -0400

    adding custom link component

--- app/index.js
+++ app/index.js
@@ -15,11 +15,28 @@ var ProfilePic = React.createClass({
   }
 });
 
+var Link = React.createClass({
+  changeURL: function () {
+    window.location.replace(this.props.href);
+  },
+
+  render: function () {
+    return (
+      <span style={{color: 'blue', cursor: 'pointer'}} onClick={this.changeURL}>
+        {this.props.children}
+      </span>
+    );
+  }
+});
+
+
 var ProfileLink = React.createClass({
   render: function () {
     return (
       <div>
-        <a href={"https://www.github.com/" + this.props.username}>{this.props.username}</a>
+        <Link href={"https://www.github.com/" + this.props.username}>
+          {this.props.username}
+        </Link>
       </div>
     );
   }
