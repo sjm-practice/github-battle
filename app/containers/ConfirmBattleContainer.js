2929f152aa522d0f7fdd7710e2fafb25b11b3cd9
stevenjmarsh <sjm.atl@gmail.com>
2016-09-09 17:56:49 -0400

    adding properly displaying github user detail

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -41,6 +41,7 @@ var ConfirmBattleContainer = React.createClass({
     return (
       <ConfirmBattle
         isLoading={this.state.isLoading}
+        onInitiateBattle={this.handleInitiateBattle}
         playersInfo={this.state.playersInfo}
       />
     );
