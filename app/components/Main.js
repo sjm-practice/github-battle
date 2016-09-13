ca4f1faf94afe4968255582040eeedb746a18e3d
stevenjmarsh <sjm.atl@gmail.com>
2016-09-13 14:08:12 -0400

    adding loading css, via webpack

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var React = require('react');
+require("../main.css"); // webpack (with style-loader & css-loader) allow you to directly require css
 
 var Main = React.createClass({
   render: function () {
