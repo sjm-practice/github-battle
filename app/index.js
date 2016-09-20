858f7b0a951fe0c4fc134e084fdeddb2220f3e07
stevenjmarsh <sjm.atl@gmail.com>
2016-09-20 11:31:09 -0400

    restoring (re-adding) sentry

--- app/index.js
+++ app/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import routes from "./config/routes";
-// import Raven from "raven-js";
+import Raven from "raven-js";
 
-// const sentryKey = "eea94840f0ae401b80a6baa48ca6a418";
-// const sentryApp = "97496";
-// const sentryURL = "https://" + sentryKey + "@sentry.io/" + sentryApp;
-//
-// Raven.config(sentryURL).install();
+const sentryKey = "eea94840f0ae401b80a6baa48ca6a418";
+const sentryApp = "97496";
+const sentryURL = "https://" + sentryKey + "@sentry.io/" + sentryApp;
+
+Raven.config(sentryURL).install();
 
 ReactDOM.render(
   routes,
