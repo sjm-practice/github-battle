f381e074398a4badeebcdd93a6837cd89c70ee45
stevenjmarsh <sjm.atl@gmail.com>
2016-09-20 11:41:31 -0400

    refactoring sentry url creation, to use string templating

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ import Raven from "raven-js";
 
 const sentryKey = "eea94840f0ae401b80a6baa48ca6a418";
 const sentryApp = "97496";
-const sentryURL = "https://" + sentryKey + "@sentry.io/" + sentryApp;
+const sentryURL = `https://${sentryKey}@sentry.io/${sentryApp}`;
 
 Raven.config(sentryURL).install();
 
